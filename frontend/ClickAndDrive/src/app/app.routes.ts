d811e2398ddbe21d73aa6984942473195a9bf092
Pavle Maksimović <makspavle@gmail.com>
2025-12-23 18:20:14 +0100

    Added routes in app.routes.ts

--- frontend/ClickAndDrive/src/app/app.routes.ts
+++ frontend/ClickAndDrive/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
 
 export const routes: Routes = [];
+=======
+import { MapViewComponent } from './components/map-view/map-view';
+import { DriverHistory } from './layout/driver-history/driver-history';
+import { MainPageComponent } from './main-page/main-page';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'map', pathMatch: 'full' },
+  { path: 'map', component: MapViewComponent },
+  { path: 'driver-history', component: DriverHistory },
+  { path: 'scheduled-rides', component: MapViewComponent },
+  { path: 'change-info', component: MapViewComponent },
+  { path: 'reports', component: MapViewComponent },
+  { path: 'notes', component: MapViewComponent },
+  { path: 'support', component: MapViewComponent },
+  //{ path: 'login', component: LoginPage },
+];
+>>>>>>> Stashed changes
--- frontend/ClickAndDrive/src/app/main-page/main-page.ts
+++ frontend/ClickAndDrive/src/app/main-page/main-page.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { MapViewComponent } from '../components/map-view/map-view';
+<<<<<<< Updated upstream
+=======
+import { RouterOutlet } from '@angular/router';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [MapViewComponent], 
+=======
+  imports: [MapViewComponent, RouterOutlet],
+>>>>>>> Stashed changes
   templateUrl: './main-page.html',
   styleUrl: './main-page.css'
 })
