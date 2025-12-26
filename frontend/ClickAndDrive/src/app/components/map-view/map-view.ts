9b157ed4ff58f686c97928dedda6c26a09e123e1
ribicnikola <165227622+ribicnikola@users.noreply.github.com>
2025-12-26 12:49:49 +0100

    Simple map implementation.
    Simple map implementation with possibility of changing road colors in the future.

--- frontend/ClickAndDrive/src/app/components/map-view/map-view.ts
+++ frontend/ClickAndDrive/src/app/components/map-view/map-view.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, AfterViewInit } from '@angular/core';
 import { RidePopup } from '../../shared/ride-popup';
+
+declare var mapboxgl: any;
 
 @Component({
   selector: 'app-map-view',
@@ -8,7 +10,7 @@ import { RidePopup } from '../../shared/ride-popup';
   templateUrl: './map-view.html',
   styleUrl: './map-view.css',
 })
-export class MapViewComponent {
+export class MapViewComponent implements AfterViewInit {
   constructor(public ridePopup: RidePopup) {}
 
   showRideData = signal(false);
@@ -21,5 +23,16 @@ export class MapViewComponent {
   onOverlayClick() {
     this.showRideData.set(false);
     this.ridePopup.close();
+  }
+
+  ngAfterViewInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoicmliaWNuaWtvbGEiLCJhIjoiY21qbTJvNHFlMmV6OTNncXhpOGNiaTVnayJ9.Bhzo0Euk2D923K3smmoVaQ';
+
+    new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v11',
+      center: [19.847781672927088, 45.23576711328475],
+      zoom: 13
+    });
   }
 }
