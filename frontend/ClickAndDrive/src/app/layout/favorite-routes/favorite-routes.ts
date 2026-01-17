6b87a1cb9a91d4fd3dfcff3d3c944637a6771323
ltopic764 <ltopic764@gmail.com>
2026-01-17 19:42:34 +0100

    Begging work on favorite routes page
    This page will contain a list of users favorite routes, displayed in cards.
    
    For now we have the background design, as well as the main container where the routes will go.

---
+++ frontend/ClickAndDrive/src/app/layout/favorite-routes/favorite-routes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-routes',
+  imports: [],
+  templateUrl: './favorite-routes.html',
+  styleUrl: './favorite-routes.css',
+})
+export class FavoriteRoutes {
+
+}
