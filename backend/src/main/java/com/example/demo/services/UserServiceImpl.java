231a292c288fb8065e180741e992f1a04c7da80f
ribicnikola <165227622+ribicnikola@users.noreply.github.com>
2026-01-28 01:11:58 +0100

    Update UserServiceImpl.java

--- backend/src/main/java/com/example/demo/services/UserServiceImpl.java
+++ backend/src/main/java/com/example/demo/services/UserServiceImpl.java
@@ -24,7 +24,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
