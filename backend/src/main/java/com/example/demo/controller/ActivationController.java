d6ff30de19f6a3ba2736325b826e88bc63e023b1
ribicnikola <165227622+ribicnikola@users.noreply.github.com>
2026-02-08 22:17:08 +0100

    Update

--- backend/src/main/java/com/example/demo/controller/ActivationController.java
+++ backend/src/main/java/com/example/demo/controller/ActivationController.java
@@ -4,12 +4,14 @@ import com.example.demo.model.Passenger;
 import com.example.demo.repositories.PassengerRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/activation")
 public class ActivationController {
 
     private final PassengerRepository passengerRepository;
